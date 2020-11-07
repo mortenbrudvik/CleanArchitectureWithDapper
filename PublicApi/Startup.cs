32da48b75bf7ec8e53dfffcf14c323984a8dffd4
Morten Brudvik <morten@brudvik.com>
2020-11-07 16:23:20 +0100

    Added swagger support for the api

--- PublicApi/Startup.cs
+++ PublicApi/Startup.cs
@@ -22,11 +22,20 @@ namespace PublicApi
             services.AddInfrastructure(Configuration);
 
             services.AddControllers();
+            services.AddMvc();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
