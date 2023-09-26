d365d20aa509bc8ebcf1565831d35b6443275bac
Morten Brudvik <morten@brudvik.com>
2023-09-26 08:32:20 +0200

    chore: Add react project

---
+++ src/TaskManager.UI/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Task Manager</h2>
+
+    </div>
+  );
+}
+
+export default App;
